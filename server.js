6599f10bf987656ed39b1d338af87acb43c8f238
alie <anthonylie94@gmail.com>
2023-04-01 18:05:28 +0800

    MVC added, with get and post api created

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const express = require("express")
-const app = express() 
+const express = require("express");
+const app = express();
 // initializing backend
 
-require("dotenv").config()
-let dbConnect = require("./config/dbConnect")
+require("dotenv").config();
+let dbConnect = require("./dbConnect");
 
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
-    res.json({message: "Doing miniproj 3"})
+  res.json({ message: "Doing miniproj 3" });
 });
+
+let userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
 
 // set port, listen for requests
 const port = process.env.port || 8080;
 app.listen(port, () => {
-    console.log(`Running on ${port}`);
-})
+  console.log(`Running on ${port}`);
+});
