b597c815f92d8f1d85f9c528c19eebe2eddb1b64
alie <anthonylie94@gmail.com>
2023-04-03 21:04:50 +0800

    added crud functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 "use strict";
 const Models = require("../models");
+
 const getUsers = (res) => {
   Models.User.findAll({})
     .then(function (data) {
@@ -9,6 +10,18 @@ const getUsers = (res) => {
       throw err;
     });
 };
+
+const getUser = (req, res) => {
+  console.log(res);
+  Models.User.findOne({ where: { id: req.params.id } })
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
 const createUsers = (data, res) => {
   Models.User.create(data)
     .then(function (data) {
@@ -18,7 +31,31 @@ const createUsers = (data, res) => {
       throw err;
     });
 };
+
+const updateUser = (data, res) => {
+  Models.User.update(data, { where: { id: data.id } })
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+const deleteUser = (req, res) => {
+  Models.User.destroy({ where: { id: req.params.id } })
+    .then(function (data) {
+      res.send(`Data of ${req.params.id} is deleted.`);
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
 module.exports = {
   getUsers,
+  getUser,
   createUsers,
+  updateUser,
+  deleteUser,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,8 +6,28 @@ router.get("/", (req, res) => {
   Controllers.userController.getUsers(res);
 });
 
+router.get("/:id", (req, res) => {
+  console.log(req, res);
+  Controllers.userController.getUser(req, res);
+});
+
 router.post("/create", (req, res) => {
-  Controllers.userController.createUsers(req.body, res);
+  Controllers.userController.createUsers(req.body, res); //
+});
+
+// router.put("/:id", (req, res) => {
+//   console.log(req.params, req.body);
+//   Controllers.userController.updateUser(req, res);
+// });
+
+router.put("/", (req, res) => {
+  console.log(req.params, req.body);
+  Controllers.userController.updateUser(req.body, res);
+});
+
+router.delete("/delete/:id", (req, res) => {
+  console.log(req, res);
+  Controllers.userController.deleteUser(req, res);
 });
 
 module.exports = router;
